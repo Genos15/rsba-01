2c3beb265b3d729aa8c956ed06b2ff4990614a23
Arnaud <cynapsenjoh@gmail.com>
2021-10-08 15:50:36 +0300

    fixing bug on creation of paramaters

--- order-microservice/src/main/kotlin/com/rsba/order_microservice/database/ParameterQueries.kt
+++ order-microservice/src/main/kotlin/com/rsba/order_microservice/database/ParameterQueries.kt
@@ -2,15 +2,17 @@ package  com.rsba.order_microservice.database
 
 
 import com.rsba.order_microservice.domain.input.ParameterInput
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.util.*
 
 object ParameterQueries {
 
     fun createOrEdit(input: ParameterInput, token: UUID) =
-        "SELECT on_create_or_edit_parameter('$input', '$token')"
+        "SELECT on_create_or_edit_parameter('${Json.encodeToString(input)}', '$token')"
 
     fun addOrRemovePotentialValue(input: ParameterInput, token: UUID) =
-        "SELECT on_add_or_remove_value_parameter('$input', '$token')"
+        "SELECT on_add_or_remove_value_parameter('${Json.encodeToString(input)}', '$token')"
 
     fun delete(input: UUID, token: UUID) =
         "SELECT on_delete_parameter('$input', '$token')"
