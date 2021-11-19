0d02455e0249f60bb9bd61635c1f665b5fccfa16
Genos15 <cynapsenjoh@gmail.com>
2021-11-19 10:05:20 +0300

    fix data type of created_at, edited_at in group

--- component-microservice/src/main/kotlin/com/rsba/component_microservice/data/dao/DepartmentDao.kt
+++ component-microservice/src/main/kotlin/com/rsba/component_microservice/data/dao/DepartmentDao.kt
@@ -1,11 +1,13 @@
 package com.rsba.component_microservice.data.dao
 
+import com.example.ticketApp.deserializer.DateTimeSerializer
 import com.rsba.component_microservice.configuration.deserializer.UUIDSerializer
 import com.rsba.component_microservice.domain.format.ModelType
 import com.rsba.component_microservice.domain.format.ModelTypeCase
 import com.rsba.component_microservice.domain.model.Group
 
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 import java.util.*
 
 @Serializable
@@ -14,8 +16,8 @@ data class DepartmentDao(
     @Serializable(with = UUIDSerializer::class) override val id: UUID,
     val name: String,
     val description: String? = null,
-    val createdAt: String? = null,
-    val editedAt: String? = null,
+    @Serializable(with = DateTimeSerializer::class) val createdAt: OffsetDateTime? = null,
+    @Serializable(with = DateTimeSerializer::class) val editedAt: OffsetDateTime? = null,
     val priority: Int? = 0,
     val isAnalytic: Boolean? = null,
     val isStaging: Boolean? = null,
--- component-microservice/src/main/kotlin/com/rsba/component_microservice/domain/model/Group.kt
+++ component-microservice/src/main/kotlin/com/rsba/component_microservice/domain/model/Group.kt
@@ -1,8 +1,10 @@
 package com.rsba.component_microservice.domain.model
 
+import com.example.ticketApp.deserializer.DateTimeSerializer
 import com.rsba.component_microservice.configuration.deserializer.UUIDSerializer
 
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 import java.util.*
 
 @Serializable
@@ -10,8 +12,8 @@ data class Group(
     @Serializable(with = UUIDSerializer::class) val id: UUID,
     val name: String,
     val description: String? = null,
-    val createdAt: String? = null,
-    val editedAt: String? = null,
+    @Serializable(with = DateTimeSerializer::class) val createdAt: OffsetDateTime? = null,
+    @Serializable(with = DateTimeSerializer::class) val editedAt: OffsetDateTime? = null,
     val parent: Group? = null,
     val priority: Int? = 0,
     val isAnalytic: Boolean? = null,
