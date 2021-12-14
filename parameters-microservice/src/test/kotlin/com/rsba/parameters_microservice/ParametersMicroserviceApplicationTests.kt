3a3df46d75b7eb6f379c2c24dc6f02acefe3187f
Genos15 <cynapsenjoh@gmail.com>
2021-12-14 12:12:22 +0100

    still

--- parameters-microservice/src/test/kotlin/com/rsba/parameters_microservice/ParametersMicroserviceApplicationTests.kt
+++ parameters-microservice/src/test/kotlin/com/rsba/parameters_microservice/ParametersMicroserviceApplicationTests.kt
@@ -1,13 +1,7 @@
 package com.rsba.parameters_microservice
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ParametersMicroserviceApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
 }
